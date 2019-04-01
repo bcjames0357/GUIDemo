33180161befe131a9437fcb957687602d6304cbf
brandon <brandon@rozie-pc>
2019-03-31 23:21:01 -0500

    Added the ListView and TextArea objects

---
+++ src/guidemo/ExampleOfTableViewController.java
@@ -0,0 +1,75 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package guidemo;
+
+import java.io.IOException;
+import java.net.URL;
+import java.time.LocalDate;
+import java.time.Month;
+import java.util.ResourceBundle;
+import javafx.collections.FXCollections;
+import javafx.collections.ObservableList;
+import javafx.event.ActionEvent;
+import javafx.fxml.FXML;
+import javafx.fxml.FXMLLoader;
+import javafx.fxml.Initializable;
+import javafx.scene.Node;
+import javafx.scene.Parent;
+import javafx.scene.Scene;
+import javafx.scene.control.TableColumn;
+import javafx.scene.control.TableView;
+import javafx.scene.control.cell.PropertyValueFactory;
+import javafx.stage.Stage;
+
+/**
+ * FXML Controller class
+ *
+ * @author brandon
+ */
+public class ExampleOfTableViewController implements Initializable {
+    
+    @FXML private TableView<Person> tableView;
+    @FXML private TableColumn<Person, String> firstNameColumn;
+    @FXML private TableColumn<Person, String> lastNameColumn;
+    @FXML private TableColumn<Person, LocalDate> birthdayColumn;
+
+    public void changeScreenButtonPushed(ActionEvent event) throws IOException
+    {
+        Parent tableViewParent = FXMLLoader.load(getClass().getResource("FXMLDocument.fxml"));
+        Scene tableViewScene = new Scene(tableViewParent);
+        
+        Stage window = (Stage) ((Node)event.getSource()).getScene().getWindow();
+        
+        window.setScene(tableViewScene);
+        window.show();
+        
+    }
+    
+    /**
+     * Initializes the controller class.
+     */
+    @Override
+    public void initialize(URL url, ResourceBundle rb) {
+        firstNameColumn.setCellValueFactory(new PropertyValueFactory<Person, String>("firstName"));
+        lastNameColumn.setCellValueFactory(new PropertyValueFactory<Person, String>("lastName"));
+        birthdayColumn.setCellValueFactory(new PropertyValueFactory<Person, LocalDate>("birthday"));
+        
+        tableView.setItems(getPeople());
+    }    
+    
+    public ObservableList<Person> getPeople()
+    {
+        ObservableList<Person> people = FXCollections.observableArrayList();
+        people.add(new Person("Frank","Sinatra",LocalDate.of(1915,Month.DECEMBER, 12)));
+        people.add(new Person("Rebecca","Fergusson",LocalDate.of(1986,Month.JULY, 21)));
+        people.add(new Person("Mr.","T",LocalDate.of(1952,Month.MAY, 21)));
+        
+        return people;        
+    }
+    
+    
+    
+}
--- src/guidemo/FXMLDocumentController.java
+++ src/guidemo/FXMLDocumentController.java
@@ -5,11 +5,17 @@
  */
 package guidemo;
 
+import java.io.IOException;
 import java.net.URL;
 import java.util.ResourceBundle;
 import javafx.collections.ObservableList;
+import javafx.event.ActionEvent;
 import javafx.fxml.FXML;
+import javafx.fxml.FXMLLoader;
 import javafx.fxml.Initializable;
+import javafx.scene.Node;
+import javafx.scene.Parent;
+import javafx.scene.Scene;
 import javafx.scene.control.CheckBox;
 import javafx.scene.control.ChoiceBox;
 import javafx.scene.control.ComboBox;
@@ -19,6 +25,7 @@ import javafx.scene.control.RadioButton;
 import javafx.scene.control.SelectionMode;
 import javafx.scene.control.TextArea;
 import javafx.scene.control.ToggleGroup;
+import javafx.stage.Stage;
 
 /**
  *
@@ -46,6 +53,19 @@ public class FXMLDocumentController implements Initializable {
     
     @FXML private ListView listView;
     @FXML private TextArea golfTextArea; 
+    
+    public void changeScreenButtonPushed(ActionEvent event) throws IOException
+    {
+        Parent tableViewParent = FXMLLoader.load(getClass().getResource("ExampleOfTableView.fxml"));
+        Scene tableViewScene = new Scene(tableViewParent);
+        
+        Stage window = (Stage) ((Node)event.getSource()).getScene().getWindow();
+        
+        window.setScene(tableViewScene);
+        window.show();
+        
+    }
+    
     
     public void choiceBoxButtonPushed()
     {
